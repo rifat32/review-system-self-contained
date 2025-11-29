ce6d7bf67adc0aeae1014740b8de891012df719c
rifat32 <rifatbilalphilips@gmail.com>
2025-11-29 13:55:26 +0600

    review model updated

--- app/Models/ReviewNew.php
+++ app/Models/ReviewNew.php
@@ -24,7 +24,12 @@ class ReviewNew extends Model
         "ip_address",
         "is_overall",
         'staff_id',
-        'order_no'
+        'order_no',
+          'sentiment_score',
+                'topics', 
+                'moderation_results',
+                'ai_suggestions',
+                'staff_suggestions'
 
     ];
       protected $casts = [
