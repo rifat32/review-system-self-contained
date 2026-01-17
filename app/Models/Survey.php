ce63caaf8c60a70cc1584db72ffa0cafe398b16f
rifat32 <rifatbilalphilips@gmail.com>
2026-01-18 00:07:03 +0600

    feat: Add client business details API endpoint and update API documentation tags.

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
  * @OA\Schema(
  *     schema="Survey",
  *     type="object",
