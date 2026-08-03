c7af4a0b04afb29ed84261e68302aaa4e8f2512e
MD. Rony Mia <rony.mia7800@gmail.com>
2026-08-03 13:07:43 +0600

    feat: update SuperAdminSeeder to support seeding multiple superadmin accounts

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -17,29 +17,43 @@ class SuperAdminSeeder extends Seeder
      */
     public function run()
     {
-        $superadmin_data = [
-            'email' => "asjadtariq@gmail.com",
-            'password' => Hash::make('12345678@We'),
-            'first_Name' => 'Asjad',
-            'phone' => null,
-            'last_Name' => 'Tariq',
-            "type" => "superadmin",
-            'remember_token' => Str::random(10),
-            'email_verified_at' => now(),
+        $superadmins = [
+            [
+                'email' => "asjadtariq@gmail.com",
+                'password' => Hash::make('12345678@We'),
+                'first_Name' => 'Asjad',
+                'phone' => null,
+                'last_Name' => 'Tariq',
+                "type" => "superadmin",
+                'remember_token' => Str::random(10),
+                'email_verified_at' => now(),
+            ],
+            [
+                'email' => "kids20acc@gmail.com",
+                'password' => Hash::make('12345678@We'), // Default password, change as needed
+                'first_Name' => 'Super',
+                'phone' => null,
+                'last_Name' => 'Admin',
+                "type" => "superadmin",
+                'remember_token' => Str::random(10),
+                'email_verified_at' => now(),
+            ]
         ];
 
-        $admin_exists = User::where('email', $superadmin_data['email'])->exists();
+        foreach ($superadmins as $superadmin_data) {
+            $admin_exists = User::where('email', $superadmin_data['email'])->exists();
 
-        if (!$admin_exists) {
-            $user = User::create($superadmin_data);
+            if (!$admin_exists) {
+                $user = User::create($superadmin_data);
 
-            // Create superadmin role if it doesn't exist
-            if (!Role::where('name', 'superadmin')->exists()) {
-                Role::create(['name' => 'superadmin', 'guard_name' => 'api']);
+                // Create superadmin role if it doesn't exist
+                if (!Role::where('name', 'superadmin')->exists()) {
+                    Role::create(['name' => 'superadmin', 'guard_name' => 'api']);
+                }
+
+                // Assign superadmin role to the user
+                $user->assignRole(User::USER_ROLE['SUPER_ADMIN']);
             }
-
-            // Assign superadmin role to the user
-            $user->assignRole(User::USER_ROLE['SUPER_ADMIN']);
         }
     }
 }
